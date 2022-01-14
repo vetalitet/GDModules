a478501ef623b25aeeab7e03544f11e54bc10c5d
Vitalii Bilov <vbey@softserveinc.com>
2022-01-14 22:53:27 +0200

    -'

--- themes/src/main/java/com/vetalitet/themes/ThemeProvider.kt
+++ themes/src/main/java/com/vetalitet/themes/ThemeProvider.kt
@@ -1,5 +1,24 @@
 package com.vetalitet.themes
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 object ThemeProvider {
+
+    private val _theme = MutableLiveData<Theme?>()
+    val theme : LiveData<Theme?> = _theme
+
+    /**
+     * maintain a HashMap of [Theme.id] and [Theme]
+     */
+    private var loadedThemes: Theme? = null
+
+    /**
+     * add or replace a [Theme]
+     */
+    fun setTheme(newTheme: Theme) {
+        loadedThemes = newTheme
+        _theme.postValue(loadedThemes)
+    }
 
 }
